bc4d7197dd38a1cebbd0c106d1713cf89b875184
Felipe Pires Iasbik <felipeiasbik@hotmail.com>
2023-04-19 15:50:29 -0300

    Sistema de cadastro

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import joi from "joi";
+import bcrypt from "bcrypt";
 import { MongoClient } from "mongodb";
 
 const app = express();
@@ -20,9 +22,33 @@ try {
 }
 const db = mongoClient.db();
 
-app.post("/", async (req, res) => {
+const signUpSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(3).required()
+});
+
+app.post("/cadastro", async (req, res) => {
+
+    const {name, email, password} = req.body;
+    const signUp = {name, email, password};
+
+    const validation = signUpSchema.validate(signUp, {aboutEarly: false});
+
+    if (validation.error) {
+        const errors = validation.error.details.map( err => err.message);
+        return res.status(422).send(errors);
+    }
 
     try {
+
+        const userExists = await db.collection("users").findOne({email});
+        if (userExists) return res.status(409).send("E-mail já cadastrado!");
+
+        const passCrypt = bcrypt.hashSync(password, 10);
+
+        await db.collection("users").insertOne({name, email, password: passCrypt});
+        res.sendStatus(201);
         
     } catch (err) {
         res.status(500).send(err.message);        
@@ -30,8 +56,19 @@ app.post("/", async (req, res) => {
 
 });
 
-app.get("/", async (req, res) => {
+app.get("/cadastro", async (req, res) => {
 
+    const {name} = req.headers;
+    console.log(name)
+
+    try {
+        const users = await db.collection("users").find().toArray();
+        res.send(users);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+
+    
 });
 
 const PORT = 5000;
